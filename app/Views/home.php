7b72252299deb4c53e358223c45812675947fdfa
Wayan-Goura <iwayangedegourasakti@gmail.com>
2025-10-09 21:42:28 +0800

    update tampilan home

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,6 @@
 <?= view('layouts/header')?>
 
-<P>go home</P>
+<P>go home anjayyyy</P>
 <ul>
     <?php foreach ($items as $item): ?>
         <li><?= $item['name'] ?> - <?= $item['description'] ?></li>
